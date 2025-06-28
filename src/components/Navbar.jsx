7f013840f457e09c4f38aa947e0b9c19414c5648
Aryankarmur <154599865+Aryankarmur@users.noreply.github.com>
2025-06-28 12:12:06 +0530

    modify conflict

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { RiAccountCircle2Line } from "react-icons/ri";
-import "../assets/css/Navbar.css";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-
-const Navbar = () => {
-  return (
-    <>
-      <div className="navbar">
-            <div className="container">
-              <div className="logo">
-                <h2>Logo</h2>
-                  </div>
-                  <div className="search">
-                      <input type="search" name="" id="" />
-                        <button>Search</button>
-                  </div>
-              <div className="nav-links">
-                <ul>
-                          <li className="profile" title="Profile"><a href="#"> <RiAccountCircle2Line />
-                    Profile</a></li>
-                    <li className="cart" title="Cart"><a href="#"><AiOutlineShoppingCart />
-                    
-                    </a></li>
-                </ul>
-              </div>
-            </div>
-      </div>
-    </>
-  )
-}
-
-export default Navbar
-=======
 import { IoSearch } from "react-icons/io5";
 import { BsCart2 } from "react-icons/bs";
 import { IoMenu } from "react-icons/io5";
@@ -207,4 +173,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
->>>>>>> a6c1ffcf728e3ad77688f1544bdde548c184c40b
